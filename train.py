d1ebba6ffb54e7bb91120df75b720f924e6d070c
slasket <askebay11@gmail.com>
2021-12-07 15:19:54 +0100

    Early Stopping

--- train.py
+++ train.py
@@ -65,7 +65,7 @@ def train_net(net,
     ''')
 
     # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
-    optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.9)
+    optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.99)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)  # goal: maximize Dice score
     grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
     criterion = nn.CrossEntropyLoss()
@@ -136,6 +136,18 @@ def train_net(net,
                             'epoch': epoch,
                             **histograms
                         })
+                        # early stopping WIP
+                        if epoch >= 1:
+                            if val_score < bestSeenDiceVal:
+                                logging.info('Dice val didnt improve so we exit')
+                                #exit(0)
+                            else:
+                                bestSeenDiceVal = val_score
+                                # save the model where we preform better
+                                Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
+                                torch.save(net.state_dict(),
+                                           str(dir_checkpoint / 'best_saveD{}.pth'.format(val_score)))
+                                logging.info(f'Checkpoint {epoch + 1} saved!')
 
         if save_checkpoint:
             Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
@@ -146,11 +158,11 @@ def train_net(net,
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
+    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=20, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=0.00001,
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
-    parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
+    parser.add_argument('--scale', '-s', type=float, default=0.1, help='Downscaling factor of the images')
     parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
                         help='Percent of the data that is used as validation (0-100)')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
