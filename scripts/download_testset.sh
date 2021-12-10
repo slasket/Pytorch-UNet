8c81043df8b1465810cfc0ea42b909566847b50e
slasket <askebay11@gmail.com>
2021-12-10 15:51:36 +0100

    Bash script for downloading the test_hq images

--- scripts/download_testset.sh
+++ scripts/download_testset.sh
@@ -11,6 +11,6 @@ echo "{\"username\":\"$USERNAME\",\"key\":\"$APIKEY\"}" > ~/.kaggle/kaggle.json
 
 kaggle competitions download -c carvana-image-masking-challenge -f test_hq.zip
 unzip test_hq.zip
-mv test/* data/test/
-rm -d test
+mv test_hq/* data/test/
+rm -d test_hq
 rm test_hq.zip
